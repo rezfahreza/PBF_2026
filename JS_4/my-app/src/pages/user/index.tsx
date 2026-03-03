777f649dd95b47626e9a8f258b0c746ddf9aeb94
Fahreza <fahreza200412@gmail.com>
2026-03-03 20:39:59 +0700

    JS4_Tugas3

--- JS_4/my-app/src/pages/user/index.tsx
+++ JS_4/my-app/src/pages/user/index.tsx
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect,useState } from "react";
+
 const UserSettingPage = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const {push} = useRouter();
+    useEffect(() => {
+        if (!isLogin) {
+            push("/auth/login");
+        }
+    }, []);
     return (
         <div>
             User Setting Page
