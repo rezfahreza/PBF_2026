b5855ea6aa8bf08d89e7175f251ccb1ba2c3452f
Fahreza <fahreza200412@gmail.com>
2026-03-10 06:42:33 +0700

    Praktikum_JS7

---
+++ JS_7/my-app/src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function About() {
+    return (
+        <div>
+            <h1>Ini Halaman About</h1>          
+        </div>
+    )
+}
---
+++ JS_7/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { use, useEffect, useState } from "react";
+
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    size: string;
+};
+
+const kategori = () => {
+
+    const[products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/produk")
+            .then((responses) => responses.json())
+            .then((responsesdata) => setProducts(responsesdata.data))
+            .catch((error) => console.error("Error fetching products: ", error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Daftar Produk</h1>
+            {products.map((products: ProductType) => (
+                <div key={products.id}>
+                    <h2>{products.name}</h2>
+                    <p>Harga: Rp {products.price}</p>
+                    <p>Ukuran: {products.size}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default kategori;
