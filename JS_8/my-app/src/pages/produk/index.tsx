0fed5472faea3cda5c073a3166560022f025111d
Fahreza <fahreza200412@gmail.com>
2026-04-04 10:47:48 +0700

    laporan JS 8

--- JS_8/my-app/src/pages/produk/index.tsx
+++ JS_8/my-app/src/pages/produk/index.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 import styles from "./product.module.scss";
+import fetcher from "../utils/swr/fetcher";
 
 type ProductType = {
   id: string;
@@ -11,34 +12,20 @@ type ProductType = {
 };
 
 const Kategori = () => {
-  const [products, setProducts] = useState<ProductType[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
 
-  const fetchProducts = () => {
-    setLoading(true);
+  const products: ProductType[] = data?.data || [];
 
-    fetch("/api/produk")
-      .then((res) => res.json())
-      .then((resData) => {
-        setProducts(resData.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  if (error) {
+    return <p>Gagal mengambil data</p>;
+  }
 
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
 
       <div className={styles.produk__content}>
-        {loading
+        {isLoading
           ? Array.from({ length: 4 }).map((_, index) => (
               <div key={index} className={styles.produk__content__skeleton}>
                 <div className={styles.produk__content__skeleton__image + " " + styles["animate-pulse"]}></div>
@@ -48,12 +35,15 @@ const Kategori = () => {
               </div>
             ))
           : products.map((product) => (
-              // Menambahkan inline width agar lebarnya sama dengan skeleton
-              <div key={product.id} className={styles.produk__content__item} style={{ width: "200px" }}>
-                <img 
-                  src={product.image} 
-                  alt={product.name} 
-                  className={styles.produk__content__item__image} 
+              <div
+                key={product.id}
+                className={styles.produk__content__item}
+                style={{ width: "200px" }}
+              >
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className={styles.produk__content__item__image}
                 />
                 <h2 className={styles.produk__content__item__name}>
                   {product.name}
